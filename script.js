8ba41ad291f4e34ae52ce477e678f0fc63d61085
simo-l <73710022+simo-l@users.noreply.github.com>
2021-02-12 10:01:00 +0100

    Fix ReadMe v.1.3

--- script.js
+++ script.js
@@ -880,7 +880,8 @@ document.body.onkeyup = function(e) {
 readmeButton.onclick = function() {
     const contenitore = document.getElementById("readMeSection");
     if (contenitore == null) {
-        const testo = "Harmonic Tension Visualizer";
+        const testo = "Harmonic Tension Visualizer.<br> The goal of our project is to analyze a chord sequence, given by the user through an intuitive piano-roll interface, in order to determine the trend of the harmonic tension. Following different criteria, estabilished by us and based on music theory, we assign to each chord a certain level of tension, influenced by:" +
+            "<br>-the chord 's composition,<br>-well-known progression patterns,<br>-the harmonic context.";
         const readMeSection = document.createElement("div");
         readMeSection.classList.add("readStyle");
         readMeSection.innerHTML += testo;
