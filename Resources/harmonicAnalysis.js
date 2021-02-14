8cac7011763f9939be3c26a648709179c370f2b1
FrancescoZumo <francescozumo@gmail.com>
2021-02-14 14:30:42 +0100

    reorganize harmonicAnalysis

--- Resources/harmonicAnalysis.js
+++ Resources/harmonicAnalysis.js
@@ -1,19 +1,132 @@
-/** DEFINITIONS: type of chords accepted
- * '' = major triad
- * min = minor triad
- * dim = diminished triad
- * aug = augmented triad
- * maj7 = major seventh
- * min7 = minor seventh
- * 7 = dominant
- * halfdim = half diminished
- * dim7 = diminished quadriad
+/** OVERVIEW:
+ * 
  */
 
-/** ASSUMPTIONS (may be modified in future updates)
- * 1) only triads and quadriads
- * 2) 
- */
+// Harmony analysis
+// - quadriadi + tese di triadi
+// - raggruppare pattern per scala?
+// - diatonicFunction solo controllo per scala maggiore	FATTO
+// - harmony analysis cambia pattern/sostituzioni considerate in base a a scala
+// - pattern cambiano tensione in base a triade o quadriade (II V I triade è meno teso della corrispondente quadriade)
+// - cerca sostituzioni e interscambi modali FATTO
+// - se arrivo da un 2, il 5 è potenziato di tensione	FATTO
+// - dovrei fare modo di non controllare i gradi assoluti, ma solo il rapporto tra toniche IMPORTANTE
+
+
+/** Main function*/
+export function harmonyAnalysis(progression){
+
+	/** PHASE 1): select keys with highest number of compatible chords*/
+	let accepted_keys = findKey(progression); // array with selected keys
+	if (accepted_keys.length == 0) {
+		throw "no key was given";
+	}
+	let progression_plus;
+	let temp;
+	
+	/** PHASE 2): choose the key with highest number of correct chords before the first wrong one*/
+	// for each accepted_key
+	for (let i = 0; i < accepted_keys.length; i++) {
+		// exception: give priority to major and minor scales
+		if (modes[accepted_keys[i].scale_index].tonal_harmony) {
+			accepted_keys[i].points+= 10;
+		}
+		// estimate relative degrees and coherence of each chord in the progression 
+		progression_plus = getProgDegrees(progression, accepted_keys[i]);
+		
+		//for each chord
+		for (let j = 0; j < progression.length; j++) {
+			// if I find a chord not coherent with the current accepted key
+			if (! (progression_plus[j].type_coherent && progression_plus[j].degree_coherent))
+				break;
+			accepted_keys[i].points++;
+		}
+	}
+	accepted_keys.sort((a, b) => (a.points > b.points) ? -1 : 1);
+
+	/** PHASE 3): analyze each "wrong" chord, based on the chosen key, with different options*/ 
+	progression_plus = getProgDegrees(progression, accepted_keys[0]);
+	
+	// array of all keys that may be found during analysis 
+	let priority_keys = [];
+	priority_keys.push(accepted_keys[0]);
+	
+	for (let i = 0; i < progression_plus.length; i++) {
+		if (!( progression_plus[i].type_coherent && progression_plus[i].degree_coherent)) {
+			
+			
+			// review!!!
+			if (progression_plus[i].type == "7") {
+				/** OPTION C): CHANGE OF SCALE *//*
+				temp = findChangeKey(progression, priority_keys, progression_plus, i);
+				if (temp) {
+					progression_plus = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}*/
+				
+				/** OPTION A): CHORD SUBSTITUTION*/
+				temp = findSubs(progression, priority_keys, progression_plus[i], i);
+				if (temp) {
+					progression_plus[i] = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}
+				
+				/** OPTION B): MODAL INTERCHANGE */
+				temp = findModalInterchange(progression, priority_keys, progression_plus[i], i);
+				if (temp) {
+					progression_plus[i] = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}
+				/** OPTION C): CHANGE OF SCALE */
+				temp = findChangeKey(progression, priority_keys, progression_plus, i);
+				if (temp) {
+					progression_plus = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}
+			}
+			else {
+				
+				/** OPTION A): CHORD SUBSTITUTION*/
+				temp = findSubs(progression, priority_keys, progression_plus[i], i);
+				if (temp) {
+					progression_plus[i] = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}
+				
+				/** OPTION B): MODAL INTERCHANGE */
+				temp = findModalInterchange(progression, priority_keys, progression_plus[i], i);
+				if (temp) {
+					progression_plus[i] = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}
+				/** OPTION C): CHANGE OF SCALE */
+				temp = findChangeKey(progression, priority_keys, progression_plus, i);
+				if (temp) {
+					progression_plus = temp;
+					priority_keys.push(temp.curr_key);
+					continue;
+				}
+			}
+			/** OPTION D): GENERAL CHORD OUT OF KEY*/
+			progression_plus[i].surprise = "D";
+			progression_plus[i].event = "out of key";
+		}
+	}
+	
+	console.log("progression analyzed: ", progression_plus);
+	
+	/** TENSION PROGRESSION */
+	progression_plus = evaluateTension(progression_plus);
+	
+	return progression_plus;
+	
+}
 
 // patterns for accepted chords
 const note_notation = /^[A-G][#b]?$/;
@@ -616,127 +729,4 @@ function evaluateTension(progression_plus){
 	return progression_plus;
 }
 
-/** Main function*/
-export function harmonyAnalysis(progression){
 
-	/** PHASE 1): select keys with highest number of compatible chords*/
-	let accepted_keys = findKey(progression); // array with selected keys
-	if (accepted_keys.length == 0) {
-		throw "no key was given";
-	}
-	let progression_plus;
-	let temp;
-	
-	/** PHASE 2): choose the key with highest number of correct chords before the first wrong one*/
-	// for each accepted_key
-	for (let i = 0; i < accepted_keys.length; i++) {
-		// exception: give priority to major and minor scales
-		if (modes[accepted_keys[i].scale_index].tonal_harmony) {
-			accepted_keys[i].points+= 10;
-		}
-		// estimate relative degrees and coherence of each chord in the progression 
-		progression_plus = getProgDegrees(progression, accepted_keys[i]);
-		
-		//for each chord
-		for (let j = 0; j < progression.length; j++) {
-			// if I find a chord not coherent with the current accepted key
-			if (! (progression_plus[j].type_coherent && progression_plus[j].degree_coherent))
-				break;
-			accepted_keys[i].points++;
-		}
-	}
-	accepted_keys.sort((a, b) => (a.points > b.points) ? -1 : 1);
-
-	/** PHASE 3): analyze each "wrong" chord, based on the chosen key, with different options*/ 
-	progression_plus = getProgDegrees(progression, accepted_keys[0]);
-	
-	// array of all keys that may be found during analysis 
-	let priority_keys = [];
-	priority_keys.push(accepted_keys[0]);
-	
-	for (let i = 0; i < progression_plus.length; i++) {
-		if (!( progression_plus[i].type_coherent && progression_plus[i].degree_coherent)) {
-			
-			
-			// review!!!
-			if (progression_plus[i].type == "7") {
-				/** OPTION C): CHANGE OF SCALE *//*
-				temp = findChangeKey(progression, priority_keys, progression_plus, i);
-				if (temp) {
-					progression_plus = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}*/
-				
-				/** OPTION A): CHORD SUBSTITUTION*/
-				temp = findSubs(progression, priority_keys, progression_plus[i], i);
-				if (temp) {
-					progression_plus[i] = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}
-				
-				/** OPTION B): MODAL INTERCHANGE */
-				temp = findModalInterchange(progression, priority_keys, progression_plus[i], i);
-				if (temp) {
-					progression_plus[i] = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}
-				/** OPTION C): CHANGE OF SCALE */
-				temp = findChangeKey(progression, priority_keys, progression_plus, i);
-				if (temp) {
-					progression_plus = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}
-			}
-			else {
-				
-				/** OPTION A): CHORD SUBSTITUTION*/
-				temp = findSubs(progression, priority_keys, progression_plus[i], i);
-				if (temp) {
-					progression_plus[i] = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}
-				
-				/** OPTION B): MODAL INTERCHANGE */
-				temp = findModalInterchange(progression, priority_keys, progression_plus[i], i);
-				if (temp) {
-					progression_plus[i] = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}
-				/** OPTION C): CHANGE OF SCALE */
-				temp = findChangeKey(progression, priority_keys, progression_plus, i);
-				if (temp) {
-					progression_plus = temp;
-					priority_keys.push(temp.curr_key);
-					continue;
-				}
-			}
-			/** OPTION D): GENERAL CHORD OUT OF KEY*/
-			progression_plus[i].surprise = "D";
-			progression_plus[i].event = "out of key";
-		}
-	}
-	
-	console.log("progression analyzed: ", progression_plus);
-	
-	/** TENSION PROGRESSION */
-	progression_plus = evaluateTension(progression_plus);
-	
-	return progression_plus;
-	
-}
-
-// Harmony analysis
-// - quadriadi + tese di triadi
-// - raggruppare pattern per scala?
-// - diatonicFunction solo controllo per scala maggiore	FATTO
-// - harmony analysis cambia pattern/sostituzioni considerate in base a a scala
-// - pattern cambiano tensione in base a triade o quadriade (II V I triade è meno teso della corrispondente quadriade)
-// - cerca sostituzioni e interscambi modali FATTO
-// - se arrivo da un 2, il 5 è potenziato di tensione	FATTO
-// - dovrei fare modo di non controllare i gradi assoluti, ma solo il rapporto tra toniche IMPORTANTE
